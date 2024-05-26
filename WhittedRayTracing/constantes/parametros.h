1902100140975f4f516e251593f1029b80de6317
nicoalenuo <nicoalenuo@gmail.com>
2024-05-26 20:25:21 -0300

    correccion de tipo

--- WhittedRayTracing/constantes/parametros.h
+++ WhittedRayTracing/constantes/parametros.h
@@ -7,6 +7,6 @@ const int IMAGEN_WIDTH = 1280;
 const int IMAGEN_HEIGHT = 720;
 
 const int PROFUNDIDAD_MAXIMA = 5; // Profunidad maxima en recursion de ray tracing
-const int EPSILON = 0.001; // Para mitigar colisiones de rayos con su lugar de partida
+const float EPSILON = 0.001f; // Para mitigar colisiones de rayos con su lugar de partida
 
 #endif
