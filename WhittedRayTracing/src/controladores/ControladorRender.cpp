890692a0f36c0757e68bd07dfd5768c3f7542300
KevinMacTec <kevinmactec@gmail.com>
2024-06-05 16:02:42 -0300

    antialiasing, necesito confirmacion

--- WhittedRayTracing/src/controladores/ControladorRender.cpp
+++ WhittedRayTracing/src/controladores/ControladorRender.cpp
@@ -189,6 +189,23 @@ imagen* ControladorRender::whitted_ray_tracing() {
 				vector_3((float)(j - imagen_width / 2), (float)(i - imagen_height / 2), plano.get_z()) - origen);
 
 			img_resultado->set_pixel(i, j, traza_rr(Rayo, 1));
+			if (ANTIALIASING) {
+				rayo AntiAliasing1 = rayo(origen, vector_3(Rayo.get_direccion().get_x() - DESPLAZAMIENTO_ANTIALIASING, Rayo.get_direccion().get_y() - DESPLAZAMIENTO_ANTIALIASING, Rayo.get_direccion().get_z()));
+				rayo AntiAliasing2 = rayo(origen, vector_3(Rayo.get_direccion().get_x() - DESPLAZAMIENTO_ANTIALIASING, Rayo.get_direccion().get_y() + DESPLAZAMIENTO_ANTIALIASING, Rayo.get_direccion().get_z()));
+				rayo AntiAliasing3 = rayo(origen, vector_3(Rayo.get_direccion().get_x() + DESPLAZAMIENTO_ANTIALIASING, Rayo.get_direccion().get_y() - DESPLAZAMIENTO_ANTIALIASING, Rayo.get_direccion().get_z()));
+				rayo AntiAliasing4 = rayo(origen, vector_3(Rayo.get_direccion().get_x() + DESPLAZAMIENTO_ANTIALIASING, Rayo.get_direccion().get_y() + DESPLAZAMIENTO_ANTIALIASING, Rayo.get_direccion().get_z()));
+				color AA1 = traza_rr(AntiAliasing1, 1);
+				color AA2 = traza_rr(AntiAliasing2, 1);
+				color AA3 = traza_rr(AntiAliasing3, 1);
+				color AA4 = traza_rr(AntiAliasing4, 1);
+				/*
+				AA1.imprimir();
+				AA2.imprimir();
+				AA3.imprimir();
+				AA4.imprimir();
+				*/
+				img_resultado->set_pixel(i, j, (img_resultado->get_pixeles()[i][j] + AA1 + AA2 + AA3 + AA4) * (1.f/5.f));
+			}
 		}
 	}
 
